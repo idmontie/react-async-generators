3a341456d0e31b850ceb47fa8f0499b5b659588b
Ivan Montiel <idmontie@gmail.com>
2021-08-08 13:02:04 -0700

    Continue to bootstrap and refactor

--- examples/src/todo/todosStore.ts
+++ examples/src/todo/todosStore.ts
@@ -1,33 +1,5 @@
-import {createCache} from "react-async-generators";
+import {createCacheWithStorage} from "react-async-generators";
 import {Todo} from "./types";
-
-// TODO use from next version of react-async-generators
-function createCacheWithStorage<T>(
-	storageKey: string,
-	defaultValue: T,
-	storage: Storage = localStorage,
-) {
-	const possibleValue = storage.getItem(storageKey) || "{}";
-	let initialValue = defaultValue;
-
-	try {
-		initialValue = JSON.parse(possibleValue);
-
-		if (!(Array.isArray(initialValue) && initialValue.length)) {
-			initialValue = defaultValue;
-		}
-	} catch (e) {
-		initialValue = defaultValue;
-	}
-
-	const {cache, addListener, ...rest} = createCache<T>(initialValue);
-
-	addListener(() => {
-		storage.setItem(storageKey, JSON.stringify(cache.value.get()));
-	});
-
-	return {cache, addListener, ...rest};
-}
 
 export const {cache: todoCache, getValue: getTodosCache} =
 	createCacheWithStorage<Todo[]>("todo-mvc", []);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+// Types
+export * from "./types";
+
+// Core
 export * from "./AsyncGenerator";
 export {default as Async} from "./AsyncGenerator";
 export {default} from "./AsyncGenerator";
+
+// Helpers
 export {mutable} from "./helpers/mutable";
 export * from "./helpers/cache";
 export * from "./helpers/createCacheWithStorage";
